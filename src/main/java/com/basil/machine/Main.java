1ea0984e67261f387ac84b1c16761c16a0eb24c9
HBwilliam <williamhabberjam@gmail.com>
2026-01-30 16:56:39 +0100

    init

---
+++ src/main/java/com/basil/machine/Main.java
@@ -0,0 +1,19 @@
+package com.basil.machine;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class Main extends JavaPlugin {
+
+    public Main(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+
+}
